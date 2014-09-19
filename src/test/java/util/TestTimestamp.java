81d8718f2a1b4319e9ba58c9b54eec2de09f233a
Austin Wang <austinmsn@hotmail.com>
2014-09-19 09:30:01 +0800

    test timestamp

---
+++ src/test/java/util/TestTimestamp.java
@@ -0,0 +1,25 @@
+package test.java.util;
+
+import java.sql.Timestamp;
+
+public class TestTimestamp {
+
+	public static void main(String[] args) {
+		Long nowL = System.currentTimeMillis();
+		
+		Long test1 = nowL % (3L*60L*60L*1000L);
+		System.out.println(test1);
+		System.out.println(new Timestamp(test1));
+		
+		Long test2 = nowL - test1;
+		System.out.println(test2);
+		System.out.println(new Timestamp(test2));
+		
+		Long test3 = nowL - test1 + (1L*60L*60L*1000L);
+		System.out.println(test3);
+		System.out.println(new Timestamp(test3));
+		
+		
+		
+	}
+}
