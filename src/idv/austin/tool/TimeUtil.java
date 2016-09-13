5f724c1ddac478b2db3711e123e8d885880fd514
austin-wang <austin-wang@nec.com.tw>
2016-09-13 12:34:18 +0800

    時間處理修正，另外把相關的方法做整理。

--- src/idv/austin/tool/TimeUtil.java
+++ src/idv/austin/tool/TimeUtil.java
@@ -1,50 +1,87 @@
-package com.nec.svc.nfa.sso.util;
+package idv.austin.tool;
 //import com.nec.svc.nfa.sso.util.TimeUtil
+
+import java.util.TreeMap;
+
 public class TimeUtil{
 	
-	private static Long jumperTime = System.nanoTime();
 	private static String logMsg = "TimeUtil.";
 
 	private static TreeMap<String, Long> timeMap = new TreeMap<String, Long>();
 
-	public Long periodTime(String key){
-		retrn expiriedTime(key, Long.MAX_VALUE);
+	public static Long periodTime(String key){
+		return expiriedTime(key, 0L);
 	}
 	
-	public Long expiriedTime(String key, Long expired){
+	public static Long expiriedTime(String key, Long expired){
+		
 		Long now = System.nanoTime();
 		
+		Long period = 0L;
+		
+		//initial key & value = now
 		Long value = timeMap.get(key);
 		if(value == null){
 			timeMap.put(key, now);
+			showMiliTime("period", period);
 			showMiliTime("first time");
 			return 0L;
 		}
-
-		Long period = now-value; 
-		showMiliTime("period", period);
-		showMiliTime("expired", expired);
 		
-		if(expired < period){
-			showMiliTime("EXPIRED RESET TIME");
+		//has value
+		period = now-value;
+//		showMiliTime("now", now);
+//		showMiliTime("value", value);
+		showMiliTime("period", period);
+		
+		boolean isExpired = period > expired;
+		showMiliTime(period+">"+expired+" for "+isExpired);
+		if(isExpired){
 			timeMap.put(key, now);
-			return 0L;
-		}else{
-			showMiliTime("IN PERIOD");
-			return period;
+			period = 0L;
 		}
+		return period;
 		
 	}
 	
-	public String showMiliTime(String msg, Long nanoTime){
-		Long miliTime = nanoTime / 1000L;
+	private static void showMiliTime(String msg, Long nanoTime){
+		Long miliTime = nanoTime;
 		showMiliTime(msg+":"+miliTime);
 	}
 	
-	private String showMiliTime(String msg){
-		System.out(logMsg+msg+);
+	private static void showMiliTime(String msg){
+		System.out.println(logMsg+msg);
 	}
 	
+	public static void main(String args[]) throws InterruptedException{
+		TimeUtil test = new TimeUtil();
+		test.runTest1();
+		System.out.println("[END]");
+		test.runTest2();
+		System.out.println("[END]");
+	}
+	
+	public static void runTest1() throws InterruptedException{
+		periodTime("test1");
+		Thread.sleep(1000);
+		periodTime("test1");
+		Thread.sleep(1000);
+		periodTime("test1");
+		Thread.sleep(1000);
+		periodTime("test1");
+	}
+	
+	public static void runTest2() throws InterruptedException{
+		expiriedTime("test2", 3000000000L);
+		Thread.sleep(1000);
+		expiriedTime("test2", 3000000000L);
+		Thread.sleep(1000);
+		expiriedTime("test2", 3000000000L);
+		Thread.sleep(1000);
+		expiriedTime("test2", 3000000000L);
+		Thread.sleep(1000);
+		expiriedTime("test2", 3000000000L);
+	}
 	
 	
 }
